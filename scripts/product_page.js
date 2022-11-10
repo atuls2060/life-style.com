3f157e542f683dfae6730b54cc9f8de8d814b419
VishnuAshok18 <vishnu.ashok181997@gmail.com>
2022-11-10 22:01:19 +0530

    completed the product page

--- scripts/product_page.js
+++ scripts/product_page.js
@@ -27,3 +27,27 @@ btnRight.onclick=()=>{
 btnLeft.onclick=()=>{
     document.getElementById('slider').scrollLeft +=180
 }
+
+
+let plus=document.querySelector(".plus")
+let minus=document.querySelector(".minus")
+let num=document.querySelector(".num")
+
+
+console.log(plus)
+let a=1;
+ 
+plus.addEventListener("click", ()=>{
+    a++;
+    a=(a<10) ?  +a:a;
+    num.innerHTML=a;
+})
+
+minus.addEventListener("click", ()=>{
+    if(a>1){
+        a--;
+        a=(a<10) ?  +a:a;
+        num.innerHTML=a;
+
+    }
+})
