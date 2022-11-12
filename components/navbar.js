347e0987e0721b2841aafc77986fe25045b5f9e9
VishnuAshok18 <vishnu.ashok181997@gmail.com>
2022-11-12 14:17:28 +0530

    still working on all product page

--- components/navbar.js
+++
@@ -1,9 +0,0 @@
-const navbar = () =>{
-    return <div></div>
-    <div>
-        
-    </div>
-    <div></div>
-}
-
-export default navbar;
--- scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('geekcoud_btn').addEventListener('click',apply);
-
-function apply(){
-    event.preventDefault();
-    console.log('function check')
-}
